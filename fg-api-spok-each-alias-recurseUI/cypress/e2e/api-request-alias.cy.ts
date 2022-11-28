5371e77c23d7e2704518f79d7079971b832713e3
Murat Ozcan <muratkerem@gmail.com>
2022-11-28 08:03:45 -0600

    5b

--- fg-api-spok-each-alias-recurseUI/cypress/e2e/api-request-alias.cy.ts
+++ fg-api-spok-each-alias-recurseUI/cypress/e2e/api-request-alias.cy.ts
@@ -1,4 +1,4 @@
-import 'cypress-aliases/commands/request'
+import 'cypress-aliases/commands'
 
 it('makes API requests using the previous value alias', () => {
   cy.request('POST', '/reset', { todos: [] })
@@ -31,6 +31,90 @@ describe('try with before block', () => {
     cy.request('PATCH', '/todos/@id', {
       completed: true,
     })
+
+    cy.request('GET', '/todos/@id')
+      .its('body')
+      .as('todo')
+      .should('deep.eq', {
+        title: 'test todo',
+        id: 1,
+        completed: true,
+      })
+  })
+})
+
+describe('try with beforeEach', () => {
+  beforeEach(() => {
+    cy.request('POST', '/reset', { todos: [] })
+    cy.request('POST', '/todos', {
+      title: 'test todo',
+      completed: false,
+    })
+      .its('body.id')
+      .as('id')
+  })
+
+  it('created the correct todo', () => {
+    // request the Todo item
+    // confirm the response body includes the correct title
+    cy.request('GET', '/todos/@id')
+      .its('body')
+      .as('todo')
+      .should('deep.eq', {
+        title: 'test todo',
+        id: 1,
+        completed: false,
+      })
+  })
+
+  it('completes the created todo', () => {
+    // update the created todo item, set the "completed: true"
+    // using the "PATCH /todos/:id" request
+    cy.request('PATCH', '/todos/@id', { completed: true })
+
+    cy.request('GET', '/todos/@id')
+      .its('body')
+      .as('todo')
+      .should('deep.eq', {
+        title: 'test todo',
+        id: 1,
+        completed: true,
+      })
+  })
+})
+
+// We want to create the Todo item once and then keep the aliased value.
+// Cypress automatically resets the aliased values,
+// but cypress-aliases implements cy.as(aliasName, { keep: true })
+// to automatically restore that alias before each test.
+describe('try with before + keep value', () => {
+  before(() => {
+    cy.request('POST', '/reset', { todos: [] })
+    cy.request('POST', '/todos', {
+      title: 'test todo',
+      completed: false,
+    })
+      .its('body.id')
+      .as('id', { keep: true })
+  })
+
+  it('created the correct todo', () => {
+    // request the Todo item
+    // confirm the response body includes the correct title
+    cy.request('GET', '/todos/@id')
+      .its('body')
+      .as('todo')
+      .should('deep.eq', {
+        title: 'test todo',
+        id: 1,
+        completed: false,
+      })
+  })
+
+  it('completes the created todo', () => {
+    // update the created todo item, set the "completed: true"
+    // using the "PATCH /todos/:id" request
+    cy.request('PATCH', '/todos/@id', { completed: true })
 
     cy.request('GET', '/todos/@id')
       .its('body')
